746a763832ab62ced845670ba57bdfb481d75a6c
Anna McDougald <texas.tiel@gmail.com>
2016-09-08 20:49:11 -0500

    More fiddling with Spring

--- NachaSDPayments/src/Controllers/DatabaseController.java
+++ NachaSDPayments/src/Controllers/DatabaseController.java
@@ -1,5 +1,16 @@
 package Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
 
-public class DatabaseController {
-
+@Controller
+public class DatabaseController{
+ 
+   @RequestMapping(value = "/getHello")
+   public Test printHello() {
+      Test temp = new Test();
+      temp.setStr("hello");
+      return temp;
+   }
 }
---
+++ NachaSDPayments/src/Controllers/Test.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+public class Test {
+	private String str;
+	
+	public Test(){}
+	
+	public void setStr(String s){ str = s; }
+	public String getStr(){ return str; }
+}
