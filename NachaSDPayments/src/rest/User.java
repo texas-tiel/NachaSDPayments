3f9d9e09625173434f187e109d69550809ed4479
Sriven <sriven.p@gmail.com>
2016-11-27 20:27:50 -0600

    Machine Learning Attributes Added Added Eligibility Function to Database Controller Updated XML and HBM FIles for Hibernate

--- NachaSDPayments/src/rest/User.java
+++ NachaSDPayments/src/rest/User.java
@@ -1,5 +1,7 @@
 package rest;
 
+import java.sql.Date;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,7 +15,13 @@ public class User implements java.io.Serializable {
 	private String username;
 	private String password;
 	private String ssn;
-
+	private String zipcode;
+	private Date dateofbirth;
+	private String firstname;
+	private String lastname;
+	private Double salary;
+	private int creditscore;
+	
 	public int getId() { return id; }
 	public void setId(int i) { id = i; }
 	
@@ -25,4 +33,24 @@ public class User implements java.io.Serializable {
 	
 	public String getSsn(){ return ssn; }
 	public void setSsn(String n){ ssn = n; }
+	
+	public String getZipcode() { return zipcode; }
+	public void setZipcode(String z) {zipcode =z; }
+ 	
+	public Date getDateofbirth() {return dateofbirth;}
+	public void setDateofbirth(Date dob) {dateofbirth = dob;}
+	
+	public String getFirstname() {return firstname;}
+	public void setFirstname(String fn) {firstname = fn; }
+	
+	public String getLastname() {return lastname;}
+	public void setLastname(String ln) {lastname = ln;}
+	
+	public Double getSalary() {return salary;}
+	public void setSalary(Double s) {salary = s;}
+	
+	public int getCreditscore() {return creditscore;}
+	public void setCreditscore(int cs) {creditscore = cs;}
+	
+	
 }
